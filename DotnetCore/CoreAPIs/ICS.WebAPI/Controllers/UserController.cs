50db31a51c6d978dbcfe894dd98849ce6c467775
SandeepGa <SandeepGa@SANDEEPGA>
2021-06-24 16:04:58 +0530

    Updated FIle

--- DotnetCore/CoreAPIs/ICS.WebAPI/Controllers/UserController.cs
+++ DotnetCore/CoreAPIs/ICS.WebAPI/Controllers/UserController.cs
@@ -7,85 +7,91 @@ using System.Threading.Tasks;
 
 namespace ICS.WebAPI.Controllers
 {
+    [ApiController]
+
+    [Route("[controller]")]
     public class UserController : Controller
     {
+        [HttpGet("Users")]
         // GET: UserController
-        public ActionResult Index()
+        public IActionResult Index()
         {
             UserInputModel userInputModel = new UserInputModel();
             userInputModel.Firstname = "TestUser First Name";
             userInputModel.Lastname = "TestUser Last Name";
             List<UserInputModel> users = new List<UserInputModel>();
-            return View();
+            users.Add(userInputModel);
+            return Ok(users);
         }
-
+        [HttpGet("Details")]
         // GET: UserController/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
+        [HttpPost("Create")]
         // GET: UserController/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: UserController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //// POST: UserController/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
 
-        // GET: UserController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+        //// GET: UserController/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
 
-        // POST: UserController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //// POST: UserController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
 
-        // GET: UserController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+        //// GET: UserController/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
 
-        // POST: UserController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //// POST: UserController/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
     }
 }
